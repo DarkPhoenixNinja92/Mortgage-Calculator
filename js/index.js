9c09193d6f761a6188c753cb4e8114169d7ac0cf
Shaun Pour <scpour@gmail.com>
2021-07-25 12:24:01 -0400

    2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
+let loanSize = parseInt(document.getElementById(("loan-val").value));
+let loanRate = document.getElementById(("interest-rate").value);
+let loanTerm = document.getElementById(("loan-period").value);
+
 let submitVal = () => {
-    console.log("Is this thing on?");
+    console.log(loanSize);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
